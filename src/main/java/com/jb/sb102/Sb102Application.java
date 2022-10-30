2061037257d8b64e8a322d4fadec93d069600636
anm-jubaer <anm-jubaer@digigate360.com>
2022-10-30 16:34:53 +0600

    initial commit

---
+++ src/main/java/com/jb/sb102/Sb102Application.java
@@ -0,0 +1,13 @@
+package com.jb.sb102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jb/sb102/Sb102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.sb102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
